70b7c5c2c2f8198af6d2c9812de8c8da83b163d6
Eric <erictakahashi@gmail.com>
2013-12-30 03:06:13 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuteable::Application.config.secret_token = '936d9357b74cc19317077be4d2e43aacaef2d32d0c64164716283f1e2733bdcf7527ef555157f7d2f6002344d50237963c8b34deee2d0e7b72101701bab6a52e'
