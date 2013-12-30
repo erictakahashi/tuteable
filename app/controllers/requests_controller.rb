c8074593e58b1507bb2ebe62431cf2d537bfaea9
Eric <erictakahashi@gmail.com>
2013-12-30 21:29:08 -0200

    Correction of Request Model pluralization; creation of more actions un Requests Controller and changes in index view

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,37 @@
 class RequestsController < ApplicationController
   def index
-    #@requests = Request.all
+    @requests = Request.all
+  end
+
+  def show
+    @request = Request.find(params[:id])
+  end
+
+  def new
+    @request = Request.new
+  end
+
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  def create
+    @request = Request.new(params[:request])
+
+    if @request.save
+      redirect_to @request
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @request = Request.find(params[:id])
+
+    if @request.update_attributes(params[:request])
+      redirect_to @request
+    else
+      render action: "edit"
+    end
   end
 end
